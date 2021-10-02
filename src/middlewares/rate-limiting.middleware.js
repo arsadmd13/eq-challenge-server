70f92eea7836ad6ba0d1d174e1679367a0b31687
Mohamed Arsad M <arshmda13@gmail.com>
2021-10-02 12:40:04 +0530

    Updated rate-limitter

--- src/middlewares/rate-limiting.middleware.js
+++ src/middlewares/rate-limiting.middleware.js
@@ -1,4 +1,4 @@
-const redis = require('redis')
+const redis = require('ioredis')
 
 // Credentials from redislabs.com
 const client = redis.createClient({
@@ -11,55 +11,26 @@ client.on("connect", () => {
   console.log("Connected to redis!");
 });
 
-module.exports = (req, res, next) => {
+module.exports = async (req, res, next) => {
   let ip = req.ip;
+  let count;
 
-  client.exists(ip, (err, doc) => {
-    if (err) {
-      console.log("Unable to connect to redis....")
-      return res.status(500).send("Internal Server Error");
-    }
+  try {
+    count = await client.incr(ip)
+  } catch (err) {
+    throw err;
+  }
 
-    if (doc === 1) {
-      client.get(ip, (err, doc) => {
-        if(err) {
-          return res.status(500).send("Internal Server Error");
-        }
 
-        let data = JSON.parse(doc)
-        let currentTime = new Date().getTime();
-        let timeDiff = (currentTime - data.startTime) / 60 / 1000;
-        
-        if (timeDiff >= 1) {
-          let body = {
-            'accessCount': 1,
-            'startTime': new Date().getTime()
-          }
-          client.set(ip, JSON.stringify(body))
-          next()
-        } else if (timeDiff < 1) {
-          if (data.accessCount >= 10) {
-            let cooldownTime = (60 - Math.trunc((new Date().getTime() - data.startTime)/1000))
-            return res.status(429).send({
-              message: "Rate time exceeded!",
-              cooldownTime: cooldownTime
-            });
-          } else {
-            data.accessCount++;
-            client.set(ip, JSON.stringify(data))
-            next()
-          }
-        }
-        
-      })
-    } else {
-      let userData = {
-        'accessCount': 1,
-        'startTime': new Date().getTime()
-      };
-      client.set(ip, JSON.stringify(userData))
-      next()
-    }
-
-  })
+  if(count === 1) {
+    client.expire(ip, 60);
+  } else if (count > 10) {
+    let remainingTime = await client.ttl(ip);
+    return res.status(429).send({
+      message: "Rate limit exceeded!",
+      cooldownTime: remainingTime
+    });
+  } 
+  
+  next();
 }
